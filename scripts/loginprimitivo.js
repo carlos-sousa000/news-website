d551f96c5f50cb8913491264e6789def89faa477
carlos-sousa000 <carloseduardoavila583@gmail.com>
2025-06-20 18:06:42 -0300

    more features almost complete

--- scripts/loginprimitivo.js
+++ scripts/loginprimitivo.js
@@ -9,21 +9,25 @@ document.addEventListener("DOMContentLoaded", function () {
     if (nomeSalvo) {
       nomeTexto.textContent = nomeSalvo;
       iconeUsuario.style.display = "inline-block";
+      nomeUsuarioLink.classList.add("logado");
     } else {
       nomeTexto.textContent = "Login";
       iconeUsuario.style.display = "none";
+      nomeUsuarioLink.classList.remove("logado");
     }
   }
 
   atualizarMenu();
 
+  // ir pra uma pagina ao clicar na sua conta
   nomeUsuarioLink.addEventListener("click", function (event) {
     event.preventDefault();
 
     const nomeSalvo = localStorage.getItem("nome");
 
     if (nomeSalvo) {
-      window.location.href = "pags/conta.html";
+      //window.location.href = "pags/conta.html";
+      window.location.href = "#";
     } else {
       window.location.href = "pags/forms/cadastro.html";
     }
